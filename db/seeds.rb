c3117abef44d533a364e2a25c238846f0c3bb79b
Trevor Landau <landau@users.noreply.github.com>
2020-03-29 17:10:59 -0400

    chore: update seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "time"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+checkBucket = Bucket.create(name: "Checks")
+groceriesBucket = Bucket.create(name: "Groceries")
+
+LineItem.create(
+  post_date: Time.parse("2020-10-5").iso8601,
+  amount: 500.23,
+  description: "BDay Money from Grandma",
+  bucket: checkBucket,
+)
+
+LineItem.create(
+  post_date: Time.parse("2020-10-4").iso8601,
+  amount: -10.54,
+  description: "Crappy Coffee",
+  bucket: groceriesBucket,
+)
+
+LineItem.create(
+  post_date: Time.parse("2020-10-4").iso8601,
+  amount: -10.54,
+  description: "No bucket yet",
+)
+
+LineItemAlias.create(name: "Crappy Coffee", alias: "Very Crappy Coffee")
