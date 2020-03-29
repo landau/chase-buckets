04b85705ca97dce9aca14e72c97c02eb042c8ce8
Trevor L <911603+landau@users.noreply.github.com>
2020-03-29 17:13:09 -0400

    chore: update seed script (#9)
    Co-authored-by: Trevor Landau <landau@users.noreply.github.com>

--- db/migrate/20200328183134_line_item_aliases.rb
+++ db/migrate/20200328183134_line_item_aliases.rb
@@ -1,4 +1,4 @@
-class LintItemAliases < ActiveRecord::Migration[5.2]
+class LineItemAliases < ActiveRecord::Migration[5.2]
   def change
     add_index :line_item_aliases, :name, unique: true
   end
