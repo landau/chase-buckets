afbf34cf4024a995c33040d5dc0ee32cc005806d
Trevor Landau <landau@users.noreply.github.com>
2020-04-07 07:52:03 -0400

    feat: display bucket totals

--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -20,4 +20,31 @@ RSpec.describe Bucket, type: :model do
       expect(bucket).to eq(true)
     end
   end
+
+  context "total_line_items" do
+    it "returns 0 if line items are not assigned to a bucket" do
+      bucket = Bucket.create!(name: "foo")
+      expect(bucket.total_line_items).to eq(0)
+    end
+
+    it "returns total of line item assigned to bucket" do
+      bucket = Bucket.create!(name: "foo")
+      Bucket.create!(name: "unaffected_bucket")
+
+      line_item = LineItem.create!(
+        post_date: Time.now.iso8601,
+        amount: 10,
+        description: "has bucket",
+        bucket: bucket,
+      )
+
+      LineItem.create!(
+        post_date: Time.now.iso8601,
+        amount: 10,
+        description: "unaffected_line_item",
+      )
+
+      expect(bucket.total_line_items).to eq(line_item.amount)
+    end
+  end
 end
