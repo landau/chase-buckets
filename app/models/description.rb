f6efa5ebf6995734dfdb69790d2244d7b94b568c
Trevor L <911603+landau@users.noreply.github.com>
2020-04-13 08:36:07 -0400

    fix: insert batch items within a transaction (#46)

--- app/models/description.rb
+++ app/models/description.rb
@@ -3,4 +3,16 @@ class Description < ApplicationRecord
   validates :value, presence: true, uniqueness: true
 
   scope :unassigned, -> { where(bucket_id: nil) }
+  scope :create_batch, ->(descs) {
+          existing_descs = all.pluck :value
+          new_descs = descs.uniq - existing_descs
+
+          # Wrap in transaction for more speed. Plus, it makes sense for the
+          # all or none to succeed.
+          output = nil
+          Description.transaction do
+            output = create(new_descs.map { |d| { value: d } })
+          end
+          return output
+        }
 end
--- spec/models/description_spec.rb
+++ spec/models/description_spec.rb
@@ -14,5 +14,25 @@ RSpec.describe Description, type: :model do
         expect(Description.unassigned.count).to eq(1)
       end
     end
+
+    context "create_batch" do
+      it "creates descriptions" do
+        d = ["foo", "bar"]
+        res = Description.create_batch d
+        expect(res.length).to eq(d.length)
+        expect(res.map { |d| d.value }).to eq(d)
+      end
+
+      it "creates non-existant descriptions" do
+        d = ["foo", "bar"]
+        Description.create_batch d
+
+        d2 = ["baz"]
+        res = Description.create_batch d + d2
+        expect(res.map { |d| d.value }).to eq(d2)
+
+        expect(Description.count).to eq(d.length + d2.length)
+      end
+    end
   end
 end
